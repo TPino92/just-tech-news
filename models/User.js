4e1b13f42b6608b2c7fa07617cb49624eb14b473
Trevor Pino <trevorpino@yahoo.com>
2020-10-20 10:09:21 -0600

    added password hashing at create and update

--- models/User.js
+++ models/User.js
@@ -1,27 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
 
 // define table columns and configuration
 User.init(
-    {
-      // define an id column
-      id: {
-        // use the special Sequelize DataTypes object provide what type of data it is
-        type: DataTypes.INTEGER,
-        // this is the equivalent of SQL's `NOT NULL` option
-        allowNull: false,
-        // instruct that this is the Primary Key
-        primaryKey: true,
-        // turn on auto increment
-        autoIncrement: true
-      },
-      // define a username column
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
+  {
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
       },
       // define an email column
       email: {
@@ -45,6 +46,18 @@ User.init(
       }
     },
     {
+      hooks: {
+        // set up beforeCreate lifecycle "hook" funct
+        async beforeCreate(userData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" funct
+        async beforeUpdate(updateUserData) {
+          updateUserData.password = await bycrypt.hash(updateUserData.password, 10);
+          return updateUserData;
+        }
+      },
         sequelize,
         // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
